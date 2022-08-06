4d47eaaf49c61f0776a69ef4540bae8b97c70af3
Anastasiia Haliara <an.haliara@gmail.com>
2022-08-06 21:26:47 +0300

    upd tasks

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -18,10 +18,10 @@ function onFormSubmit(e) {
   for (let position = 1; position <= amount; position += 1) {
     createPromise(position, delay)
       .then(({ position, delay }) => {
-        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
       })
       .catch(({ position, delay }) => {
-        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+        Notify.failure(`Rejected promise ${position} in ${delay}ms`);
       });
     delay += step;
   }
