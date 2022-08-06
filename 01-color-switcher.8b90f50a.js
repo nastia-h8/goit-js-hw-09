a10fd1e831a39c763e20e9af93398a22aab1a653
nastia-h8 <96496878+nastia-h8@users.noreply.github.com>
2022-08-06 13:01:14 +0000

    Deploying to gh-pages from @ nastia-h8/goit-js-hw-09@5c5887fb4230568ec6e5060458b59085ed2f547a 🚀

---
+++ 01-color-switcher.8b90f50a.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),r=document.body;t.addEventListener("click",(function(){a=setInterval((function(){r.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),e.removeAttribute("disabled"),t.setAttribute("disabled",!0)})),e.addEventListener("click",(function(){clearInterval(a),e.setAttribute("disabled",!0),t.removeAttribute("disabled")})),e.setAttribute("disabled",!0);var a=null}();
+//# sourceMappingURL=01-color-switcher.8b90f50a.js.map
